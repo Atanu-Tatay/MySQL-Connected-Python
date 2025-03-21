b398eeb99c5357e1a0f94a0bbe4e31e40158fa29
Atanu Dhara <130431465+Atanu-Tatay@users.noreply.github.com>
2025-03-21 18:14:48 +0530

    Create Show Database.py

---
+++ Show Database.py
@@ -0,0 +1,12 @@
+import mysql.connector as myconn
+mydb=myconn.connect(
+    host="localhost",         #your MySQL server address
+    user="root",                 #your MySQL username
+    password="aaaaaa“,   #your MySQL password
+    database=“Lee”          #your MySQL database
+)
+dbcursor=mydb.cursor()
+dbcursor.execute(“select * from Emp”)
+for dbshow in dbcursor.fetchall():
+      print(dbshow)
+
