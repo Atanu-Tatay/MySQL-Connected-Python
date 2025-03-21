ace8c3d7ef174247dda1ca525cac1fdca5075189
Atanu Dhara <130431465+Atanu-Tatay@users.noreply.github.com>
2025-03-21 18:11:17 +0530

    Create Delete Table.py

---
+++ Delete Table.py
@@ -0,0 +1,11 @@
+import mysql.connector as myconn
+mydb=myconn.connect(
+    host="localhost",         #your MySQL server address
+    user="root",                 #your MySQL username
+    password="aaaaaa“,   #your MySQL password
+    database=“Lee”          #your MySQL database
+)
+dbcursor=mydb.cursor()  #Create a cursor object
+dbcursor.execute(“drop table Emp") #delete a table
+print(“table created")  #Working Response
+
